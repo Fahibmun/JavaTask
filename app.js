f35119ffa6a7ede397717c844c0a29d5e1818079
Fahibmun <134392528+Fahibmun@users.noreply.github.com>
2023-09-24 08:09:54 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,237 @@
-Document.write("hello");
+// ##############################  Chapter 1 (Alerts) ################################# 
+
+// 1.	Alert these following (individually): 
+// I.	First Name
+// II.	Last Name
+// III.	Email
+// IV.	Phone Number
+// V.	Password
+
+// alert ("I.	First Name");
+// alert ("II.	Last Name");
+// alert ("III.	Email");
+// alert ("IV.	Phone Number");
+// alert ("V.	Password");
+
+// ############### 
+
+// 2.	Correct this statement: alert"You're learning JavaScript!"; 
+
+// alert ("You're learning JavaScript!");
+
+
+// 3.	Code an alert statement displaying any message you like.
+// alert("Hello World");
+
+
+
+
+// ##############################  Chapter 2 (Variables for string) ################################# 
+
+// 1.	Declare any variable in the camelCase format.
+
+// var FirstName = "";
+
+// 2.	Declare a variable of your choice without defining it. Then, in a second statement, assign it a string of your choice. 
+// FirstName = "Fahib";
+
+// 3.	Declare the variable teamName and Alert your Team name.
+
+// var teamName = "Pakistan";
+// alert (teamName);
+
+// 4.	This statement has already been coded. varbestMan = "Charlie";Assign the variable a new string.
+
+// var varbestMan = "Charlie"
+
+// varbestMan = "Irfan";
+
+
+
+// ##############################  Chapter 3 (Variables for numbers) ################################# 
+
+// 1.	Declare a variable “caseQty”
+// var caseQty = "";
+
+// 2.	Assign to the variable caseQty, which has already been declared, the value 144.
+// caseQty = 144 ;
+
+// 3.	Rewrite this statement so the variable can be used in a math operation.varnum = "9";
+
+//  var num = "9";
+
+// 5.	What is the value of orderTotal?
+// varmerchTotal = 100;
+// varshippingCharge = 10;
+// varorderTotal = merchTotal + shippingCharge;
+// Try it yourself.
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+
+// console.log (orderTotal);
+
+// 6.	In the first statement declare a variable and assign it a number. In the second statement, change the value of the variable by adding it together with a number. 
+// var num = 10;
+// num = num + 15;
+
+
+// ##############################  Chapter 4 (Variable names Legal and Illegal) ################################# 
+
+// 1.	Correct this statement.
+// var product cost = 3.45;
+
+// var product_cost = 3.45;
+
+
+// 2.	Rewrite this using camelCase.
+// varNameofband;
+  
+//  var NameOfBand;
+
+// 3.In a single statement declare a legally-named variable and assign a number to it.
+ 
+// var MyNumber = 44;
+
+// 4.	Declare a variable that is a combination of your first and last names. Use camelCase.
+// var firstName = "Fahib";
+// var lastName = "Fayyaz";
+// var fullName = firstName + lastName;
+
+
+// 5.	List the legal and Illegal Variables.
+
+//  myVar
+//  _privateVar
+//  $anotherVar
+//  camelCaseVar
+//  num123
+ 
+//  Illegal Variable Names:
+
+// 123num (Cannot start with a digit)
+// my-var (Cannot contain hyphens)
+// my var (Cannot contain spaces)
+// my!var (Cannot contain special characters other than underscores and dollar signs)
+
+
+
+// ##############################  Chapter 5 (Math Expression I) ################################# 
+
+
+// 1.	What is the name and symbol of the arithmetic operator that gives you the remainder when one number is divided by another?
+
+// var remainder = 9 % 3;   //  is the modulus operator.
+
+
+// 2.	What is the value of num?
+// varnum = 20 % 6;
+
+// num = 2 ;  
+
+// 3.	In a single statement, declare the variable largeNum and assign it the result of 1,000 multiplied by 2,000.
+
+// var largeNum = 1000 * 2000;
+
+
+// 4.	Assign to a variable the value represented by one variable subtracted from the value represented by another variable
+
+// var num1 = 20;
+// var num2 = 10;
+// var result = num1 - num2; 
+
+
+// 5.	Assign to a variable the remainder when one number is divided by another. The variable hasn't been declared beforehand. Make up the variable name.
+
+// var Num1 = 20;
+// var Num2 = 10;
+
+// var Remainder = Num1 % Num2 
+
+// alert (Remainder);
+
+// 6.	Code an alert that displays the result of a multiplication on 2 numbers.
+
+// var num1 = 3;
+// var num2 = 5;
+// var result = num1 * num2;
+
+// alert("The result of the multiplication is: " + result);
+
+
+// ##############################  Chapter 6 (Math Expression II) ################################# 
+
+// 1.	Code a short form of x = x + 1; Use either of the two legal expressions.
+
+//              x++; // Post-increment
+//              ++x; // Pre-increment
+
+// 2.	If x has a value of 100, what is the fastest way to reduce it to 99 with a math expression?
+
+//   var x = 100;
+//   var x2 = --x;
+
+
+// 3.	var x = 50;
+// var y = x++;
+// What is the value of y?
+  
+//   Value Of y = 50
+
+
+// 4.	var y = 50;
+// var z = --y;
+// What is the value of z?
+
+//    Value of Z = 49
+
+// 5.	In a single statement, decrement num and assign its original value to newNum.
+
+// var num = 40;
+// var newNum = num-- ;
+
+
+// 7.	 Assign a number value to a variable.
+// Increment the variable.
+// Display the new value in an alert
+
+// var num = 5; 
+// var newValue = ++num; 
+// alert(newValue);
+
+
+
+// ##############################  Chapter 7 (Math Expression III) ################################# 
+// 1.	var calculatedNum = 2 + (2 * 6);
+// What is the value of calculatedNum?
+
+// calculatedNum Has Value Of 14
+
+
+// 2.	var calculatedNum = (2 + 2) * 6;
+// What is the value of calculatedNum?
+
+// calculatedNum Has Value Of 24
+
+// 3.	varcalculatedNum = (2 + 2) * (4 + 2);
+// What is the value of calculatedNum?
+
+// calculatedNum Has Value Of 24
+
+// 4.	varcalculatedNum = ((2 + 2) * 4) + 2;
+// What is the value of calculatedNum?
+
+// calculatedNum Has Value Of 18
+
+
+// 5.	Write a statement that assigns to cost the result of 2 + 2 * 4 + 10, clarified with parentheses, producing 56.
+
+// var CalculateNum = (2 + 2) * (4 + 10);   
+
+// 6.	Write a statement that assigns to units the result of 2 + 2 * 4 + 10, clarified with parentheses, producing 20.
+
+// var units = 2 + (2 * 4) + 10;
+
+
+// 7.	Write a statement that assigns to pressure the result of 4 / 2 * 4, clarified with parentheses, producing 5.
